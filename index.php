64d0aac452461b7a6b6752b02e5ef23131f59e3e
smart <yiza29@yahoo.com>
2023-05-09 07:59:51 -0500

    2023-05-09

--- index.php
+++ index.php
@@ -18,48 +18,7 @@
 <body>
     <!-- Navbar -->
     <section>
-        <nav class="navbar navbar-expand-lg bg-dark navbar-dark">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="#">
-                    <img src="assets/img/logo.png" width="80" alt="">
-                </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
-                        <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">Home</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">About Us</a>
-                        </li>
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Categories
-                            </a>
-                            <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href="#">Action</a></li>
-                                <li><a class="dropdown-item" href="#">Another action</a></li>
-                                <li>
-                                    <hr class="dropdown-divider">
-                                </li>
-                                <li><a class="dropdown-item" href="#">Something else here</a></li>
-                            </ul>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Contact Us</a>
-                        </li>
-                    </ul>
-                    <form class="d-flex" role="search">
-                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                        <button class="btn btn-outline-success" type="submit">
-                            <i class="fa-solid fa-search"></i>
-                        </button>
-                    </form>
-                </div>
-            </div>
-        </nav>
+        <?php include "assets/includes/nav.php" ?>
     </section>
 
     <!-- Hero -->
@@ -74,6 +33,87 @@
         </div>
     </section>
 
+    <section>
+        
+        <div class="container-fluid w-100 hero-two-bg row">
+            <h1 class="text-center text-decoration-underline fw-bolder fs-1 text-white">
+                Apply For Jobs Here
+            </h1>
+
+            <div class="categories col-md-3 border border-5 border-secondary-subtle rounded-top m-5 bg-white text-dark">
+                <h6 class="text-uppercase text-center text-bg-dark text-white">
+                <i class="fa-solid fa-database text-white mx-2"></i> it jobs
+                </h6>    
+            </div>
+            <div class="categories col-md-3 px-2 py-2 border border-5 border-secondary-subtle rounded-top m-5 bg-white text-dark">
+                <h6 class="text-uppercase text-center text-bg-dark text-white">
+                <i class="fa-solid fa-file-invoice-dollar text-white mx-2"></i>accounting jobs
+                </h6>
+            </div>
+            <div class="categories col-md-3 px-2 py-2 border border-5 border-secondary-subtle rounded-top m-5 bg-white text-dark">
+                <h6 class="text-uppercase text-center text-bg-dark text-white">
+                <i class="fa-solid fa-gears text-white mx-2"></i></i>engineering jobs
+                </h6>
+                
+            </div>
+            <div class="categories col-md-3 px-2 py-2 border border-5 border-secondary-subtle rounded-top m-5 bg-white text-dark">
+                <h6 class="text-uppercase text-center text-bg-dark text-white">
+                <i class="fa-solid fa-headset text-white mx-2"></i>customer service jobs
+                </h6>
+                
+            </div>
+            <div class="categories col-md-3 px-2 py-2 border border-5 border-secondary-subtle rounded-top m-5 bg-white text-dark">
+                <h6 class="text-uppercase text-center text-bg-dark text-white">
+                <i class="fa-solid fa-list-check text-white mx-2"></i>front desk jobs
+                </h6>
+                
+            </div>
+            <div class="categories col-md-3 px-2 py-2 border border-5 border-secondary-subtle rounded-top m-5 bg-white text-dark">
+                <h6 class="text-uppercase text-center text-bg-dark text-white">
+                <i class="fa-regular fa-user text-white mx-2"></i>personal assistant jobs
+                </h6>
+                
+            </div>
+
+    </section>
+
+    <section>
+        <div class="btm-pg container-fluid bg-dark d-flex">
+            <div class="col-md-4 col-sm-6 col-12">
+                <p class="text-white fs-3 border border-2 border-dark w-50 text-center py-2 mt-3">
+                    Jobs
+                </p>
+                <ul class="fs-6 text-capitalize" style="list-style: none; color: white; ">
+                    <li>job bonuses</li>
+                    <li>salary jobs</li>
+                    <li>earn as you work jobs</li>
+                    <li>discounted jobs</li>
+                </ul>
+            </div>
+            <div class="col-md-4">
+                <p class="text-white fs-3 border border-2 border-dark w-50 text-center py-2 mt-3">
+                    Affiliates
+                </p>
+                <ul class="fs-6 text-capitalize" style="list-style: none; color: white; ">
+                    <li>JobSect</li>
+                    <li>Jobberman</li>
+                    <li>JobFest</li>
+                    <li>Jobbery</li>
+                    <li>JobFinder</li>
+                </ul>
+            </div>
+            <div class="col-md-4">
+                <p class="text-white fs-3 border border-2 border-dark w-50 text-center py-2 mt-3">
+                    Contact Us
+                </p>
+                <ul class="fs-6 text-capitalize" style="list-style: none; color: white; ">
+                    <li>Address:</li>
+                    <li>Phone Number</li>
+                    <li>Email Address</li>
+                </ul>
+            </div>
+        </div>
+    </section>
 
 
     <script src="assets/js/bootstrap.bundle.min.js"></script>
