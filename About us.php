744c6ec24979380a7a00fa692cee59284c8f31f5
justice369 <fridayudojustice@gmail.com>
2023-05-09 12:07:10 +0100

    about us

---
+++ About us.php
@@ -0,0 +1,157 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <title>Job Fair ~ Everybody must work</title>
+
+    <link rel="shortcut icon" href="assets/img/logo.png">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/Fontawesome/css/all.css">
+    <link rel="stylesheet" href="assets/css/index.css">
+</head>
+
+<body>
+    <!-- Navbar -->
+    <section>
+        <nav class="navbar navbar-expand-lg bg-dark navbar-dark">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">
+                    <img src="assets/img/logo.png" width="80" alt="">
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">About Us</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Categories
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="#">Action</a></li>
+                                <li><a class="dropdown-item" href="#">Another action</a></li>
+                                <li>
+                                    <hr class="dropdown-divider">
+                                </li>
+                                <li><a class="dropdown-item" href="#">Something else here</a></li>
+                            </ul>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Contact Us</a>
+                        </li>
+                    </ul>
+                    <form class="d-flex" role="search">
+                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                        <button class="btn btn-outline-success" type="submit">
+                            <i class="fa-solid fa-search"></i>
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </nav>
+    </section>
+
+<!-- about us -->
+    <section class="pt-5">
+        <div id="carouselExampleCaptions" class="carousel slide">
+            <div class="carousel-indicators">
+              <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+              <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+              <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            </div>
+            <div class="carousel-inner">
+              <div class="carousel-item active">
+                <img src="assets/img/about1.jpg" class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                  <h5> </h5>
+                  <p class=" fw-bolder fs-1 ">WE LOOK OUT FOR THE BEST.</p>
+                </div>
+              </div>
+              <div class="carousel-item">
+                <img src="assets/img/about2.jpg" class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                  <h5></h5>
+                  <p class=" fw-bolder fs-1 "> WE TRAIN AND RE-TRAIN THEM.</p>
+                </div>
+              </div>
+              <div class="carousel-item">
+                <img src="assets/img/about3.jpg" class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                  <h5></h5>
+                  <p class=" fw-bolder fs-1 ">WE INTEGRATE THEM INTO THE SYSTEM.</p>
+                </div>
+              </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Next</span>
+            </button>
+          </div>
+    </section >
+    <div>
+        <h1 class="pt-5 text-center fs-1 fw-bolder text-danger">ABOUT JOB FAIR</h1>
+<p class="text-center fs-3">Job Fair is licensed by the federal government of Nigeria as a an institution. <br>
+
+This means we are authorised to providejob services to individuals and businesses. <br>
+
+We have strong IT infrastructure and digital channels which we deploy to provide <br> exceptional service  to our customers so they can achieve their objectives.</p>
+    </div>
+
+
+
+    <section  class="pt-5">
+        <div class="container text-center ">
+            <div class="row text-start">
+              <div class="col-12 col-sm-6 col-md-4 col-lg-8   ">
+                <h1 class=" text-danger">Our Vision</h1>
+                <p class="fs-4">To build an enduring and sustainable customer-focused financial institution.</p>
+                <p><hr></p>
+                <h1 class=" text-danger">Our Mission</h1>
+                <p class="fs-4">To provide our customers with unique experience, be their preferred Job partner and nurture the attainment of their objectives</p>
+                <hr>
+                <h1 class=" text-danger">Our Business Profile</h1>
+                <p class="fs-4">JOb FAir is an innovative Job institution that provides personal, private, corporate, commercial and digital Recruitment and  Job solutions. Our tailored emploment services delivery includes: Business Advisory, Portfolio Management, Personalised Relationship Management, Fast-tracked Service delivery and Self-service solutions Training and Re- Training of staff.
+
+                    Job Fair competitive advantage in Private, Institutional, Business and Personal employment is driven by the philosophy to create support and value for Institutions, Agencies, SMEs and HNIs. Our business development strategy also focuses on developing expertise and collaborating to improve the non-oil (emerging) sector of the Nigerian Economy, which includes but not limited to Agriculture, Mining, Hospitality, E-commerce, and Art & Entertainment.
+                    
+                    At Job Fair, we believe that the New World of Fast, Smart, Personal, and Borderless Employment relationship is here. We are therefore inspired by our Future Forward jog ethos to make life (at work and leisure) more exciting for our partners with the use of cutting-edge technology that delivers best-in-class customer satisfaction.</p>
+
+              </div >
+              
+              
+              <div class="col-12 col-sm-6 col-md-4 col-lg-4  border-1 border rounded-5 mt-2 pt-2 shadow"><img src="assets/img/about4.jpg" alt=""height="80%"  width="100%"> <button type="button" class="btn btn-danger w-100 mt-4 ">CORPORATE PROFILE</button>
+                <a href=""></a><button type="button" class="btn btn-danger w-100 mt-4 ">CERTIFICATE INCORPORATION</button>
+                <a href=""></a></div> 
+            </div>
+          </div>
+    </section>
+    <section>
+        <div class="container  mt-5 mb-5 border rounded-2 p-2">
+            <img src="assets/img/about5.jpg" alt=""width= "100%" height= "400rem">
+            <p class="text-center p-2 fs-5">Our meeting rooms are available for customer's service delivery and satisfaction</p>
+        </div>
+    </section>
+
+   
+
+
+
+    <script src="assets/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
