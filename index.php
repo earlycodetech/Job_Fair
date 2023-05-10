d625100f9b80fee0bffebabcad3254c055ed6161
smart <yiza29@yahoo.com>
2023-05-10 10:14:45 -0500

    my contribution on class project

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
-    <meta name="keywords" content="">
+    <meta name="ke" content="">
     <title>Job Fair ~ Everybody must work</title>
 
     <link rel="shortcut icon" href="assets/img/logo.png">
@@ -34,83 +34,76 @@
     </section>
 
     <section>
-        
-        <div class="container-fluid w-100 hero-two-bg row">
-            <h1 class="text-center text-decoration-underline fw-bolder fs-1 text-white">
-                Apply For Jobs Here
-            </h1>
-
-            <div class="categories col-md-3 border border-5 border-secondary-subtle rounded-top m-5 bg-white text-dark">
-                <h6 class="text-uppercase text-center text-bg-dark text-white">
-                <i class="fa-solid fa-database text-white mx-2"></i> it jobs
-                </h6>    
-            </div>
-            <div class="categories col-md-3 px-2 py-2 border border-5 border-secondary-subtle rounded-top m-5 bg-white text-dark">
-                <h6 class="text-uppercase text-center text-bg-dark text-white">
-                <i class="fa-solid fa-file-invoice-dollar text-white mx-2"></i>accounting jobs
-                </h6>
-            </div>
-            <div class="categories col-md-3 px-2 py-2 border border-5 border-secondary-subtle rounded-top m-5 bg-white text-dark">
-                <h6 class="text-uppercase text-center text-bg-dark text-white">
-                <i class="fa-solid fa-gears text-white mx-2"></i></i>engineering jobs
-                </h6>
-                
-            </div>
-            <div class="categories col-md-3 px-2 py-2 border border-5 border-secondary-subtle rounded-top m-5 bg-white text-dark">
-                <h6 class="text-uppercase text-center text-bg-dark text-white">
-                <i class="fa-solid fa-headset text-white mx-2"></i>customer service jobs
-                </h6>
-                
-            </div>
-            <div class="categories col-md-3 px-2 py-2 border border-5 border-secondary-subtle rounded-top m-5 bg-white text-dark">
-                <h6 class="text-uppercase text-center text-bg-dark text-white">
-                <i class="fa-solid fa-list-check text-white mx-2"></i>front desk jobs
-                </h6>
-                
-            </div>
-            <div class="categories col-md-3 px-2 py-2 border border-5 border-secondary-subtle rounded-top m-5 bg-white text-dark">
-                <h6 class="text-uppercase text-center text-bg-dark text-white">
-                <i class="fa-regular fa-user text-white mx-2"></i>personal assistant jobs
-                </h6>
-                
-            </div>
-
+            <a href="" class="text-decoration-none apply">
+                <h1 class="text-center fw-bolder fs-1 text-dark-50">
+                    Apply For Jobs Here
+                </h1>
+            </a> 
+        <div class="container-fluid vh-100 hero-two-bg">
+            
+            <div class="suggest text-center">
+                <div class="row">
+                    <div class="col-md-4 mt-5 hover">
+                            <a href="" class="">
+                                <h3 class="text-white text-uppercase fs-1 ms-3 d-flex align-items-center justify-content-center" style="background-image: url(assets/img/it.jpg); background-size: cover; height: 250px;">
+                                    it jobs
+                            </a> 
+                    </div>
+                    <div class="col-md-4 mt-5 hover">
+                            <a href="" class="">
+                                <h3 class="text-white text-uppercase fs-1 ms-3 d-flex align-items-center justify-content-center" style="background-image: url(assets/img/engr.jpg); background-size: cover; height: 250px;">
+                                    engineering jobs
+                                </h3>
+                            </a> 
+                    </div>
+                    <div class="col-md-4 mt-5 hover">
+                            <a href="" class="">
+                                <h3 class="text-white text-uppercase fs-1 ms-3 d-flex align-items-center justify-content-center" style="background-image: url(assets/img/acct.jpg); background-size: cover; height: 250px;">
+                                    accounting jobs
+                                </h3>
+                            </a> 
+                    </div>
+                </div>
+        </div>
     </section>
 
+
     <section>
-        <div class="btm-pg container-fluid bg-dark d-flex">
-            <div class="col-md-4 col-sm-6 col-12">
-                <p class="text-white fs-3 border border-2 border-dark w-50 text-center py-2 mt-3">
-                    Jobs
-                </p>
-                <ul class="fs-6 text-capitalize" style="list-style: none; color: white; ">
-                    <li>job bonuses</li>
-                    <li>salary jobs</li>
-                    <li>earn as you work jobs</li>
-                    <li>discounted jobs</li>
-                </ul>
-            </div>
-            <div class="col-md-4">
-                <p class="text-white fs-3 border border-2 border-dark w-50 text-center py-2 mt-3">
-                    Affiliates
-                </p>
-                <ul class="fs-6 text-capitalize" style="list-style: none; color: white; ">
-                    <li>JobSect</li>
-                    <li>Jobberman</li>
-                    <li>JobFest</li>
-                    <li>Jobbery</li>
-                    <li>JobFinder</li>
-                </ul>
-            </div>
-            <div class="col-md-4">
-                <p class="text-white fs-3 border border-2 border-dark w-50 text-center py-2 mt-3">
-                    Contact Us
-                </p>
-                <ul class="fs-6 text-capitalize" style="list-style: none; color: white; ">
-                    <li>Address:</li>
-                    <li>Phone Number</li>
-                    <li>Email Address</li>
-                </ul>
+        <div class="btm-pg container-fluid bg-dark">
+            <div class="row">
+                <div class="col-md-4 col-sm-6 col-12">
+                    <p class="text-white fs-3 border border-2 border-dark w-50 text-center py-2 mt-3">
+                        Jobs
+                    </p>
+                    <ul class="fs-6 text-capitalize" style="list-style: none; color: white; ">
+                        <li>job bonuses</li>
+                        <li>salary jobs</li>
+                        <li>earn as you work jobs</li>
+                        <li>discounted jobs</li>
+                    </ul>
+                </div>
+                <div class="col-md-4">
+                    <p class="text-white fs-3 border border-2 border-dark w-50 text-center py-2 mt-3">
+                        Affiliates
+                    </p>
+                    <ul class="fs-6 text-capitalize" style="list-style: none; color: white; ">
+                        <li>JobSect</li>
+                        <li>Jobberman</li>
+                        <li>JobFest</li>
+                        <li>Jobbery</li>
+                        <li>JobFinder</li>
+                    </ul>
+                </div>
+                <div class="col-md-4">
+                    <p class="text-white fs-3 border border-2 border-dark w-50 text-center py-2 mt-3">
+                        Contact Us
+                    </p>
+                    <ul class="fs-6 text-capitalize" style="list-style: none; color: white; ">
+                        <li>Address:</li>
+                        <li>Phone Number</li>
+                        <li>Email Address</li>
+                    </ul>
+                </div>
             </div>
         </div>
     </section>
