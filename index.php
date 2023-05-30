cc9a35d34775b70d1330bd163de8619796bd9630
emma2625 <emmanuelodobo10@gmail.com>
2023-05-30 12:34:53 +0100

    update

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+<?php
+require "assets/includes/db_connect.php";
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -19,10 +22,10 @@
     <section>
         <?php include "assets/includes/nav.php" ?>
     </section>
-    
 
-  <section>
-        <div  class="container-fluid vh-100 hero-bg d-flex justify-content-center align-items-center">
+
+    <section>
+        <div class="container-fluid vh-100 hero-bg d-flex justify-content-center align-items-center">
             <div class="fs-1 fw-bold text-center">
                 <p class="text-white">Welcome to Job Fair <br> Where Everybody must work</p>
 
@@ -33,31 +36,28 @@
     </section>
 
     <section>
-            <a href="" class="text-decoration-none apply">
-                <h1 class="text-center fw-bolder fs-1 text-dark-50">
-                    Apply For Jobs Here
-                </h1>
-            </a> 
-        <div class="container-fluid vh-100 hero-two-bg">
-            
-            <div class="suggest text-center">
-                <div class="row">
-                    <div class="col-md-4 mt-5 hover">
-                            <a href="" class="text-decoration-none link-primary text-uppercase fw-bolder fs-1 ms-3 d-flex align-items-center justify-content-center" style="background-image: url(assets/img/it.jpg); background-size: cover; height: 250px;">
-                                    it jobs
-                            </a> 
-                    </div>
-                    <div class="col-md-4 mt-5 hover">
-                            <a href="" class="text-decoration-none link-warning text-uppercase fw-bolder fs-1 ms-3 d-flex align-items-center justify-content-center" style="background-image: url(assets/img/engr.jpg); background-size: cover; height: 250px;">
-                                    engineering jobs
-                            </a> 
-                    </div>
-                    <div class="col-md-4 mt-5 hover">
-                            <a href="" class="text-decoration-none link-danger text-uppercase fw-bolder fs-1 ms-3 d-flex align-items-center justify-content-center linl" style="background-image: url(assets/img/acct.jpg); background-size: cover; height: 250px;">
-                                    accounting jobs
-                            </a> 
-                    </div>
+        <a href="" class="text-decoration-none apply">
+            <h1 class="text-center fw-bolder fs-1 text-dark-50">
+                Apply For Jobs Here
+            </h1>
+        </a>
+        <div class="container-fluid py-5 hero-two-bg">
+
+            <div class="suggest">
+                <div class="row my-5">
+                    <?php
+                    $sql = "SELECT * FROM posted_jobs ORDER BY id DESC";
+                    $query = mysqli_query($dbConnect, $sql);
+                    while ($job = mysqli_fetch_assoc($query)) {
+                    ?>
+                        <div class="col-md-4 mb-3 hover">
+                            <a href="job-description?q=<?php echo $job['id']; ?>" class="text-decoration-none text-white p-2 text-uppercase fw-bolder fs-4 ms-3 d-flex align-items-center justify-content-center linl" style="background-image: url(assets/img/acct.jpg); background-size: cover; height: 250px;">
+                               <?php echo $job['title']; ?>
+                            </a>
+                        </div>
+                    <?php } ?>
                 </div>
+            </div>
         </div>
     </section>
 
